e69dce953d329ac29e051efe5d1958b64e69fa43
qingqing1997 <941355373@qq.com>
2021-02-05 11:34:27 +0800

    抽屉

--- pages/searchclue/searchclue.vue
+++ pages/searchclue/searchclue.vue
@@ -1,10 +1,23 @@
 <template>
 	<view class="content">
+		<uni-drawer ref="drawer" mode="right" :width="drawWid">
+			<view style="padding:30rpx;">
+				标签
+			</view>
+		<view style="padding:30rpx;">
+				地区
+			</view>
+			<view style="padding:30rpx;">
+				行业
+			</view>
+			<view style="padding:30rpx;">
+				来源
+			</view>
+		</uni-drawer>
 		<view class="topview">
-			<button class="search-btn">1</button>
+			<button class="search-btn"></button>
 			<input class="se-input" name="nickname" placeholder="请输入客户名称" v-model="kword" /><button type="primary" size="small"
-			 class="shai-btn" @click="getList('search')">筛选</button></button> <button type="primary" size="small" class="shai-btn"
-			 @click="add()">新增</button></view>
+			 class="shai-btn" @click="drawer()">筛选</button></button> <button type="primary" size="small" class="shai-btn" @click="add()">新增</button></view>
 		<!-- 数据列表 -->
 		<scroll-view scroll-y @scrolltolower="getNextData" class="right-part">
 			<view v-if="showxs" class="nodata-part" style="padding-top:100upx;">
@@ -38,36 +51,86 @@
 </template>
 
 <script>
+	import uniDrawer from "@/components/uni-drawer/uni-drawer.vue"
 	export default {
+		components: {
+			uniDrawer
+		},
 		data() {
 			return {
 				kword: '',
 				isAll: false,
 				nowPage: 1,
+				drawWid: '100%',
 				list: [],
 				from: 'pro_list',
 				showxs: false,
+				checkAllTag: false,
+				tagsArr: [],
+				// 全选全不选
+				ktags: [],
+				usrid: 2,
+				token: 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTI1MjE4NzksImlhdCI6MTYxMjQ4NTg3OSwiaXNzIjoiY3JtIiwiZGF0YSI6eyJpZCI6NTIsIm5hbWUiOiJcdTc2ZDhcdTUzZTRcdTZkNGJcdThiZDU1MiIsImNsb3VkX2lkIjoyLCJ1bl9pZCI6MiwidW5fbmFtZSI6Ilx1NmM4OFx1OTYzM1x1NzZkOFx1NTNlNCIsImRlX2lkIjoxMywiZGVfbmFtZSI6Ilx1NTkyN1x1NWUwMlx1NTczYVx1OTBlOCIsImRlcGFydHR5cGUiOjEwLCJwb3N0X25hbWUiOiJcdTYwM2JcdTc2ZDEiLCJsZXZlbCI6NzcsInN1cGVyX2lkIjoxNywia2EiOjAsImlwIjoiMTcyLjE4LjMuMTI5IiwibGFzdHRpbWUiOjE2MTI0ODU4MzYsInBvc3RzIjpbeyJpZCI6MCwiZG5hbWUiOiJcdTU5MjdcdTVlMDJcdTU3M2FcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIiwiZmxhZyI6MX0seyJpZCI6MSwiZG5hbWUiOiJcdTU5MjdcdTViOWVcdTY1YmRcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIiwiZmxhZyI6MH1dLCJwb3dlcnMiOiIxNzQyODgxNzk4NjEwOTg3NDE4ODU3NzI3NzM2Nzk5MTU2MTk5NDg4Mzk5Mzk3MDI3NTQwODk1NzQ2OTgxNzc0Mjk2NTg3NDM1OTI3NzE3MDA4Njc1NDUxODU0NDk4OTk3Mjg5OTgwMzEyNDAwNDQwOTQ0NTU2NDgwNjU1NTI2MzYwMjUxMzMyMzc0MTk5Njg3NTIwNzgyNjc4MjY4OTE0OTc2ODA0NTEwNzYyMzczMDI1NjE3NjI2NzEzOTQ0MzQ4Nzc0NTgwODU4MzAxMzcwNDk5MzE4MTY0MTczNjQzMDk3NDI0MjYzNzcwNDA3NzEwNjUzNjA0NTAxNjc0ODk3NjM4NDUxMTM3NDE4ODc0Njg1NzMzNzEwMDYzOTU5NTM1NDQ2MTczMTg0MzI0MzIwODg3ODAyNjgzNjQ2NTI0MjY1MTA2MjQ0MjUxMDgxNjQ1Njk2NzEyNjA2NDcxOTQxMzE2NjQ5NTcyMjE0NDM0NjE1MzcxMDQ1OTM1MTQwMzY3Mjk0MzI5OTM0NjQ5ODcwNjA3MzU5MzE1NzM4MDU1OTYzMzAzNzIyNDIyNTgyNTQzODA3ODg1NzIwOTM1OTMyMzQ0NzgxOTE1MTY2OTcyMDI1MDg3MTc0NzYwMTYxNDkyNTg3MTQyOTIxMjA1MTU0MzYxOTkxNTYzNDE3MjI1ODY1OTc5ODY4NTAwNDUwMjMzNTE0MTQzMzA2ODM3NjM3ODQzODY0NjQ3NTQxNzc2NzE5NjAwOTU4OTEzNTU0MjM4MTI2NDg4MDE4MDQ2NzY0NzY3Nzc2MzY0MTA3MjU5MzM4MDA1ODc3NTgyOTAxNzQ1MDE0MzU1Mzk3MjQ2MDAzMjkwMDczNzU5MTY2MzcxNjAwODA1MzMwOTYxMzE4NzIyNDA1OTExNDUzNTM1MjM4MDAwOTMzMjEyMjYyOTY2OTgyNTM4NzA4ODM4NTcwMDYxOTQ3Nzc5NDU2ODI3MjI5NzUwMDcxMTU3NzkxOTQ5NzA2MjA4NjE5NjY2OTE4MDg4ODkwNTU3ODg5NzkwNTUyMTc0NzI3NjM1MTYxMjE1MjQ4MTk3NTU1ODgxODc2NTc3MTQ5NzE1MjU0MTU1MzQ4Nzg1NTY4NzAwMDUxODQ4ODU4OTA2NDMxNTI1ODIwMTM5MDA3OTQxNzU4NDgwNTY0MTY3Nzc2ODIxMTAzMTg0MDc4MDIyODY4MTI5NTU2MzI1ODQ4OTM3MDA1MDU1NiJ9fQ.seS5rD1Tl4rdjq3Y2bZX8uPHemF8eDfJhZOSKg-o6LU'
 
 			}
 		},
 		onLoad(options) {
 			this.getList();
+			this.tage();
 		},
 		methods: {
+			// 全选全不选
+			handleCheckAllTagChange() {
+				this.ktags = []
+				if (this.checkAllTag) {
+					Object.keys(this.tagsArr).map((item) => {
+						this.ktags.push(parseInt(item))
+					})
+				}
+			},
+			handleCheckedTagChange() {
+				this.checkAllTag = this.ktags.length === Object.keys(this.tagsArr).length
+			},
+			//抽屉
+			drawer() {
+				this.$refs.drawer.open();
+			},
+			//标签接口
+			tage() {
+				uni.request({
+					url: this.$burl + '/api/get_tags/' + this.usrid,
+					header: {
+						'Authorization': this.token
+					},
+					success: (res) => {
+						console.log("我是标签接口", res);
+						this.tagsArr = res.data.data.data;
+						console.log("我是标签接口", this.tagsArr);
+						
+
+					},
+					fail: (err) => {
+						//console.log(err)
+					}
+				})
+			},
+
+
+			//列表接口
 			getList() {
+				uni.showLoading();
 				uni.request({
 					url: this.$burl + '/api/customer/clue/my',
-					// header: {Authorization:'JWT ' + uni.getStorageSync('token')},
 					header: {
-						'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTI0NTk0NjIsImlhdCI6MTYxMjQyMzQ2MiwiaXNzIjoiY3JtIiwiZGF0YSI6eyJpZCI6NTIsIm5hbWUiOiJcdTc2ZDhcdTUzZTRcdTZkNGJcdThiZDU1MiIsImV0b2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUoxYzJWeVgybGtJam8xTWl3aWFYTnpJam9pWlhKd0lpd2lhR1ZoWkdWeWN5STZleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjlMQ0psZUhBaU9qRTJNVEkwTlRJeE5qa3NJbWxoZENJNk1UWXhNalF5TXpNMk9Td2lkWE5sY2w5dVlXMWxJam9pWEhVM05tUTRYSFUxTTJVMFhIVTJaRFJpWEhVNFltUTFOVElpZlEuZUF6V0JrSjZuSkhPNWVweU9HcU8zOWpEcUNMTVlMV2pFTHV5a2o3YzNoQSIsImNsb3VkX2lkIjoyLCJ1bl9pZCI6MiwidW5fbmFtZSI6Ilx1NmM4OFx1OTYzM1x1NzZkOFx1NTNlNCIsImRlX2lkIjoxMywiZGVfbmFtZSI6Ilx1NTkyN1x1NWUwMlx1NTczYVx1OTBlOCIsImRlcGFydHR5cGUiOjEwLCJwb3N0X25hbWUiOiJcdTYwM2JcdTc2ZDEiLCJsZXZlbCI6NzcsInN1cGVyX2lkIjoxNywia2EiOjAsImlwIjoiMTcyLjE4LjMuMTI5IiwibGFzdHRpbWUiOjE2MTI0MjM0NjAsInBvc3RzIjpbeyJpZCI6MCwiZG5hbWUiOiJcdTU5MjdcdTVlMDJcdTU3M2FcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIn0seyJpZCI6MSwiZG5hbWUiOiJcdTU5MjdcdTViOWVcdTY1YmRcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIn1dLCJwb3dlcnMiOiIxNzQyODgxNzk4NjEwOTg3NDE4ODU3NzI3NzM2Nzk5MTU2MTk5NDg4Mzk5Mzk3MDI3NTQwODk1NzQ2OTgxNzc0Mjk2NTg3NDM1OTI3NzE3MDA4Njc1NDUxODU0NDk4OTk3Mjg5OTgwMzEyNDAwNDQwOTQ0NTU2NDgwNjU1NTI2MzYwMjUxMzMyMzc0MTk5Njg3NTIwNzgyNjc4MjY4OTE0OTc2ODA0NTEwNzYyMzczMDI1NjE3NjI2NzEzOTQ0MzQ4Nzc0NTgwODU4MzAxMzcwNDk5MzE4MTY0MTczNjQzMDk3NDI0MjYzNzcwNDA3NzEwNjUzNjA0NTAxNjc0ODk3NjM4NDUxMTM3NDE4ODc0Njg1NzMzNzEwMDYzOTU5NTM1NDQ2MTczMTg0MzI0MzIwODg3ODAyNjgzNjQ2NTI0MjY1MTA2MjQ0MjUxMDgxNjQ1Njk2NzEyNjA2NDcxOTQxMzE2NjQ5NTcyMjE0NDM0NjE1MzcxMDQ1OTM1MTQwMzY3Mjk0MzI5OTM0NjQ5ODcwNjA3MzU5MzE1NzM4MDU1OTYzMzAzNzIyNDIyNTgyNTQzODA3ODg1NzIwOTM1OTMyMzQ0NzgxOTE1MTY2OTcyMDI1MDg3MTc0NzYwMTYxNDkyNTg3MTQyOTIxMjA1MTU0MzYxOTkxNTYzNDE3MjI1ODY1OTc5ODY4NTAwNDUwMjMzNTE0MTQzMzA2ODM3NjM3ODQzODY0NjQ3NTQxNzc2NzE5NjAwOTU4OTEzNTU0MjM4MTI2NDg4MDE4MDQ2NzY0NzY3Nzc2MzY0MTA3MjU5MzM4MDA1ODc3NTgyOTAxNzQ1MDE0MzU1Mzk3MjQ2MDAzMjkwMDczNzU5MTY2MzcxNjAwODA1MzMwOTYxMzE4NzIyNDA1OTExNDUzNTM1MjM4MDAwOTMzMjEyMjYyOTY2OTgyNTM4NzA4ODM4NTcwMDYxOTQ3Nzc5NDU2ODI3MjI5NzUwMDcxMTU3NzkxOTQ5NzA2MjA4NjE5NjY2OTE4MDg4ODkwNTU3ODg5NzkwNTUyMTc0NzI3NjM1MTYxMjE1MjQ4MTk3NTU1ODgxODc2NTc3MTQ5NzE1MjU0MTU1MzQ4Nzg1NTY4NzAwMDUxODQ4ODU4OTA2NDMxNTI1ODIwMTM5MDA3OTQxNzU4NDgwNTY0MTY3Nzc2ODIxMTAzMTg0MDc4MDIyODY4MTI5NTU2MzI1ODQ4OTM3MDA1MDU1NiJ9fQ.cpwjwJRhRp5BQY4ms797NC8HMX69O0p2E1JJT6QoDrE'
+						'Authorization': this.token
 					},
 					data: {
 						// user_id: uni.getStorageSync('user_id'),
 						// type: '1'
 					},
+
 					success: (res) => {
-						console.log(res);
-						uni.showLoading();
+						uni.hideLoading();
 						if (res.statusCode == 200) {
 							this.list = res.data.data.data;
 							if (this.list.length == 0) {
@@ -88,19 +151,19 @@
 			},
 
 
-			changeTab(v) {
-				this.selectTab = v.id;
-				this.dataList = [];
-				this.isAll = false;
-				this.nowPage = 1;
-				this.getProData();
-			},
+			// changeTab(v) {
+			// 	this.selectTab = v.id;
+			// 	this.dataList = [];
+			// 	this.isAll = false;
+			// 	this.nowPage = 1;
+			// 	this.getProData();
+			// },
 			getNextData() {
 				if (this.isAll) {
 					this.showToast('已加载全部');
 				} else {
 					this.nowPage++;
-					this.getProData();
+					// this.getProData();
 				}
 			},
 			//拨打电话
@@ -254,9 +317,11 @@
 	.search-btn {
 		height: 60rpx;
 		width: 60rpx;
-		background: url(../../static/ss.png);
-		background-size: contain;
+		background: url(../../static/ss.png) no-repeat #007aff;
+		background-size: 70%;
+		background-position: center;
 		position: absolute;
+		left: 425rpx;
 	}
 
 	.shai-btn {
@@ -269,7 +334,7 @@
 
 	.topview {
 		width: 96%;
-		margin-top: 25rpx;
+		margin-top:50rpx;
 		display: flex;
 		justify-content: flex-start;
 		align-items: center;
