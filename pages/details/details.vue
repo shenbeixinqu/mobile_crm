34de86864144bedf5588fe4d2b2c25b6522823e5
qingqing1997 <941355373@qq.com>
2021-02-08 10:01:32 +0800

    新加详情页面

---
+++ pages/details/details.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			},
+			onLoad(options){
+				console.log('我是打印出来的',options)
+				
+			},
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/searchclue/searchclue.vue
+++ pages/searchclue/searchclue.vue
@@ -19,13 +19,11 @@
 				<view class="check_bj">
 					<checkbox-group class="check_box_k" @change="allChoosezt">
 						<label class="lable-box">
-							<checkbox value="all" :class="{'checked':allChecked}" :checked="allChecked?true:false"></checkbox><text class="cketext"
+							<checkbox value="all" :class="{'checked':allCheckedzt}" :checked="allCheckedzt?true:false"></checkbox><text class="cketext"
 							 style="color:rgb(64, 158, 255);">全选</text>
 						</label>
 					</checkbox-group>
 				</view>
-			
-			
 			
 				<view class="chou_tit">
 					标签
@@ -152,6 +150,7 @@
 				checkedArr: [], //复选框选中的值
 				checkedArrzt: [], //状态复选框选中的值
 				allChecked: false, //是否全选
+				allCheckedzt: false, //是否全选
 				kword: '',
 				isAll: false,
 				nowPage: 1,
@@ -243,7 +242,7 @@
 						value: "15"
 					},
 				],
-				token: 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTI3MTgwNDcsImlhdCI6MTYxMjY4MjA0NywiaXNzIjoiY3JtIiwiZGF0YSI6eyJpZCI6NTIsIm5hbWUiOiJcdTc2ZDhcdTUzZTRcdTZkNGJcdThiZDU1MiIsImV0b2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUoxYzJWeVgybGtJam8xTWl3aWFYTnpJam9pWlhKd0lpd2lhR1ZoWkdWeWN5STZleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjlMQ0psZUhBaU9qRTJNVEkzTVRBM05EY3NJbWxoZENJNk1UWXhNalk0TVRrME55d2lkWE5sY2w5dVlXMWxJam9pWEhVM05tUTRYSFUxTTJVMFhIVTJaRFJpWEhVNFltUTFOVElpZlEuNHN5MEI3cndfQ2RyRThEU2o2bl9KNDBZTGVBVVZZdzM0YV8xbnBsOHgzayIsImNsb3VkX2lkIjoyLCJ1bl9pZCI6MiwidW5fbmFtZSI6Ilx1NmM4OFx1OTYzM1x1NzZkOFx1NTNlNCIsImRlX2lkIjoxMywiZGVfbmFtZSI6Ilx1NTkyN1x1NWUwMlx1NTczYVx1OTBlOCIsImRlcGFydHR5cGUiOjEwLCJwb3N0X25hbWUiOiJcdTYwM2JcdTc2ZDEiLCJsZXZlbCI6NzcsInN1cGVyX2lkIjoxNywia2EiOjAsImlwIjoiMTcyLjE4LjMuMTI5IiwibGFzdHRpbWUiOjE2MTI2ODIwNDUsInBvc3RzIjpbeyJpZCI6MCwiZG5hbWUiOiJcdTU5MjdcdTVlMDJcdTU3M2FcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIn0seyJpZCI6MSwiZG5hbWUiOiJcdTU5MjdcdTViOWVcdTY1YmRcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIn1dLCJwb3dlcnMiOiIxNzQyODgxNzk4NjEwOTg3NDE4ODU3NzI3NzM2Nzk5MTU2MTk5NDg4Mzk5Mzk3MDI3NTQwODk1NzQ2OTgxNzc0Mjk2NTg3NDM1OTI3NzE3MDA4Njc1NDUxODU0NDk4OTk3Mjg5OTgwMzEyNDAwNDQwOTQ0NTU2NDgwNjU1NTI2MzYwMjUxMzMyMzc0MTk5Njg3NTIwNzgyNjc4MjY4OTE0OTc2ODA0NTEwNzYyMzczMDI1NjE3NjI2NzEzOTQ0MzQ4Nzc0NTgwODU4MzAxMzcwNDk5MzE4MTY0MTczNjQzMDk3NDI0MjYzNzcwNDA3NzEwNjUzNjA0NTAxNjc0ODk3NjM4NDUxMTM3NDE4ODc0Njg1NzMzNzEwMDYzOTU5NTM1NDQ2MTczMTg0MzI0MzIwODg3ODAyNjgzNjQ2NTI0MjY1MTA2MjQ0MjUxMDgxNjQ1Njk2NzEyNjA2NDcxOTQxMzE2NjQ5NTcyMjE0NDM0NjE1MzcxMDQ1OTM1MTQwMzY3Mjk0MzI5OTM0NjQ5ODcwNjA3MzU5MzE1NzM4MDU1OTYzMzAzNzIyNDIyNTgyNTQzODA3ODg1NzIwOTM1OTMyMzQ0NzgxOTE1MTY2OTcyMDI1MDg3MTc0NzYwMTYxNDkyNTg3MTQyOTIxMjA1MTU0MzYxOTkxNTYzNDE3MjI1ODY1OTc5ODY4NTAwNDUwMjMzNTE0MTQzMzA2ODM3NjM3ODQzODY0NjQ3NTQxNzc2NzE5NjAwOTU4OTEzNTU0MjM4MTI2NDg4MDE4MDQ2NzY0NzY3Nzc2MzY0MTA3MjU5MzM4MDA1ODc3NTgyOTAxNzQ1MDE0MzU1Mzk3MjQ2MDAzMjkwMDczNzU5MTY2MzcxNjAwODA1MzMwOTYxMzE4NzIyNDA1OTExNDUzNTM1MjM4MDAwOTMzMjEyMjYyOTY2OTgyNTM4NzA4ODM4NTcwMDYxOTQ3Nzc5NDU2ODI3MjI5NzUwMDcxMTU3NzkxOTQ5NzA2MjA4NjE5NjY2OTE4MDg4ODkwNTU3ODg5NzkwNTUyMTc0NzI3NjM1MTYxMjE1MjQ4MTk3NTU1ODgxODc2NTc3MTQ5NzE1MjU0MTU1MzQ4Nzg1NTY4NzAwMDUxODQ4ODU4OTA2NDMxNTI1ODIwMTM5MDA3OTQxNzU4NDgwNTY0MTY3Nzc2ODIxMTAzMTg0MDc4MDIyODY4MTI5NTU2MzI1ODQ4OTM3MDA1MDU1NiJ9fQ.7XFybI60ImVXl8IqrSz2XMNe_CrG3Gq_HPO4Gl8-krc'
+				token: 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTI3ODEyNzgsImlhdCI6MTYxMjc0NTI3OCwiaXNzIjoiY3JtIiwiZGF0YSI6eyJpZCI6NTIsIm5hbWUiOiJcdTc2ZDhcdTUzZTRcdTZkNGJcdThiZDU1MiIsImNsb3VkX2lkIjoyLCJ1bl9pZCI6MiwidW5fbmFtZSI6Ilx1NmM4OFx1OTYzM1x1NzZkOFx1NTNlNCIsImRlX2lkIjoxMywiZGVfbmFtZSI6Ilx1NTkyN1x1NWUwMlx1NTczYVx1OTBlOCIsImRlcGFydHR5cGUiOjEwLCJwb3N0X25hbWUiOiJcdTYwM2JcdTc2ZDEiLCJsZXZlbCI6NzcsInN1cGVyX2lkIjoxNywia2EiOjAsImlwIjoiMTcyLjE4LjMuMTI5IiwibGFzdHRpbWUiOjE2MTI3NDUyNjQsInBvc3RzIjpbeyJpZCI6MCwiZG5hbWUiOiJcdTU5MjdcdTVlMDJcdTU3M2FcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIiwiZmxhZyI6MX0seyJpZCI6MSwiZG5hbWUiOiJcdTU5MjdcdTViOWVcdTY1YmRcdTkwZTgiLCJuYW1lIjoiXHU2MDNiXHU3NmQxIiwiZmxhZyI6MH1dLCJwb3dlcnMiOiIxNzQyODgxNzk4NjEwOTg3NDE4ODU3NzI3NzM2Nzk5MTU2MTk5NDg4Mzk5Mzk3MDI3NTQwODk1NzQ2OTgxNzc0Mjk2NTg3NDM1OTI3NzE3MDA4Njc1NDUxODU0NDk4OTk3Mjg5OTgwMzEyNDAwNDQwOTQ0NTU2NDgwNjU1NTI2MzYwMjUxMzMyMzc0MTk5Njg3NTIwNzgyNjc4MjY4OTE0OTc2ODA0NTEwNzYyMzczMDI1NjE3NjI2NzEzOTQ0MzQ4Nzc0NTgwODU4MzAxMzcwNDk5MzE4MTY0MTczNjQzMDk3NDI0MjYzNzcwNDA3NzEwNjUzNjA0NTAxNjc0ODk3NjM4NDUxMTM3NDE4ODc0Njg1NzMzNzEwMDYzOTU5NTM1NDQ2MTczMTg0MzI0MzIwODg3ODAyNjgzNjQ2NTI0MjY1MTA2MjQ0MjUxMDgxNjQ1Njk2NzEyNjA2NDcxOTQxMzE2NjQ5NTcyMjE0NDM0NjE1MzcxMDQ1OTM1MTQwMzY3Mjk0MzI5OTM0NjQ5ODcwNjA3MzU5MzE1NzM4MDU1OTYzMzAzNzIyNDIyNTgyNTQzODA3ODg1NzIwOTM1OTMyMzQ0NzgxOTE1MTY2OTcyMDI1MDg3MTc0NzYwMTYxNDkyNTg3MTQyOTIxMjA1MTU0MzYxOTkxNTYzNDE3MjI1ODY1OTc5ODY4NTAwNDUwMjMzNTE0MTQzMzA2ODM3NjM3ODQzODY0NjQ3NTQxNzc2NzE5NjAwOTU4OTEzNTU0MjM4MTI2NDg4MDE4MDQ2NzY0NzY3Nzc2MzY0MTA3MjU5MzM4MDA1ODc3NTgyOTAxNzQ1MDE0MzU1Mzk3MjQ2MDAzMjkwMDczNzU5MTY2MzcxNjAwODA1MzMwOTYxMzE4NzIyNDA1OTExNDUzNTM1MjM4MDAwOTMzMjEyMjYyOTY2OTgyNTM4NzA4ODM4NTcwMDYxOTQ3Nzc5NDU2ODI3MjI5NzUwMDcxMTU3NzkxOTQ5NzA2MjA4NjE5NjY2OTE4MDg4ODkwNTU3ODg5NzkwNTUyMTc0NzI3NjM1MTYxMjE1MjQ4MTk3NTU1ODgxODc2NTc3MTQ5NzE1MjU0MTU1MzQ4Nzg1NTY4NzAwMDUxODQ4ODU4OTA2NDMxNTI1ODIwMTM5MDA3OTQxNzU4NDgwNTY0MTY3Nzc2ODIxMTAzMTg0MDc4MDIyODY4MTI5NTU2MzI1ODQ4OTM3MDA1MDU1NiJ9fQ.4iq_1073s9IuKT812UjeYEgRUBiZBpX7cfVWgo__Co0'
 
 			}
 		},
@@ -253,51 +252,7 @@
 			this.locations();
 			this.industrys();
 		},
-		//过滤器
-		filters: {
-			numToMean: function(value) {
-				let audit = ''
-				if (value === 0) {
-					audit = '--'
-					return audit
-				} else if (value === 1) {
-					audit = '体系审核通过'
-					return audit
-				} else if (value === 2) {
-					audit = '体系审核拒绝'
-					return audit
-				} else if (value === 3) {
-					audit = '总经办审核通过'
-					return audit
-				} else if (value === 4) {
-					audit = '总经办审核通过'
-					return audit
-				}
-
-				return audit
-			},
-
-			delayStatus: function(value) {
-				let delay = ''
-				if (value === "-1") {
-					delay = '--'
-					return delay
-				} else if (value === "0") {
-					delay = '待审核'
-					return delay
-				} else if (value === "1") {
-					delay = '审核通过'
-					return delay
-				} else if (value === "2") {
-					delay = '已拒绝'
-					return delay
-				}
-
-				return delay
-			},
-
-		},
-
+		
 
 		methods: {
 
@@ -332,9 +287,7 @@
 			},
 			// 多选复选框改变事件
 			changeCheckbox(e) {
-
 				this.checkedArr = e.detail.value;
-
 				// 如果选择的数组中有值，并且长度等于列表的长度，就是全选
 				if (this.checkedArr.length > 0 && this.checkedArr.length == this.checkboxData.length) {
 					this.allChecked = true;
@@ -368,17 +321,18 @@
 			
 				// 如果选择的数组中有值，并且长度等于列表的长度，就是全选
 				if (this.checkedArrzt.length > 0 && this.checkedArrzt.length == this.stagesArr.length) {
-					this.allChecked = true;
+					this.allCheckedzt = true;
 				} else {
-					this.allChecked = false;
+					this.allCheckedzt = false;
 				}
 			},
 			// 全选事件
-			allChoosezt(e) {
+			allChoosezt(e){
+				console.log(e);
 				let chooseItem = e.detail.value;
 				// 全选
 				if (chooseItem[0] == 'all') {
-					this.allChecked = true;
+					this.allCheckedzt = true;
 					for (let item of this.stagesArr) {
 						let itemVal = String(item.value);
 						if (!this.checkedArrzt.includes(itemVal)) {
@@ -387,16 +341,12 @@
 					}
 				} else {
 					// 取消全选
-					this.allChecked = false;
-					this.checkedArr = [];
+					this.allCheckedzt = false;
+					this.checkedArrzt = [];
 				}
 			},
 			
-			//线索来源
-			sourceChange(e) {
-				console.log(e);
-				this.source_flag = e.detail.value;
-			},
+		
 			//抽屉打开
 			drawer() {
 				this.$refs.drawer.open();
@@ -484,6 +434,7 @@
 					data: {
 						kword: this.kword,
 						ktags: this.checkedArr.join(','),
+						kstage: this.checkedArrzt.join(','),
 						kloc: this.praseStrEmpty(dq),
 						kind: this.praseStrEmpty(hy),
 
