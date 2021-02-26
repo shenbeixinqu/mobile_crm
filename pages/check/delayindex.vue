db6862be28457c58d53f4f6627212cd42d29793d
qingqing1997 <941355373@qq.com>
2021-02-26 11:43:38 +0800

    my

--- pages/check/delayindex.vue
+++ pages/check/delayindex.vue
@@ -8,8 +8,7 @@
 						<input class="uni-input" v-model="reason" name="refuseReason" placeholder="请输入拒绝原因" >
 					</view>
 					<view class="uni-btn-v">
-						<button form-type="submit" class="btn btn1">提交</button>
-						<button class="btn" @click="qx">取消</button>
+						 
 					</view>
 				</form>
 			</view>
