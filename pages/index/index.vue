607fc78af76bf9c2195776360e0304203202261b
dingshuai <1090990640@qq.com>
2021-03-05 11:06:06 +0800

    权限

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -32,12 +32,12 @@
 
 		<view class="indexconent" v-if="roles.includes('3037') || roles.includes('3028')">审核</view>
 		<view class="indeximg">
-			<view class="index_n_img" @click="track">
-				<image class="index_image" src="../../static/gjsh.png" mode="aspectFit" v-if="roles.includes('3037')"></image>
+			<view class="index_n_img" @click="track" v-if="roles.includes('3037')">
+				<image class="index_image" src="../../static/gjsh.png" mode="aspectFit" ></image>
 				<h4>跟进线索审核</h4>
 			</view>
-			<view class="index_n_img" @click="cusdelay">
-				<image class="index_image" src="../../static/wdxs.png" mode="aspectFit" v-if="roles.includes('3028')"></image>
+			<view class="index_n_img" @click="cusdelay" v-if="roles.includes('3028')">
+				<image class="index_image" src="../../static/wdxs.png" mode="aspectFit" ></image>
 				<h4>线索延期审核</h4>
 			</view>
 		</view>
